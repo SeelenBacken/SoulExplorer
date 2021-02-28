f8011235093ab98a889ee37562d8b10120660656
SeelenBacken <mail@soulback.de>
2021-02-28 13:54:44 +0100

    Base Structure and Main-Toolbar

---
+++ main.js
@@ -0,0 +1,62 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const ipcMain = electron.ipcMain;
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 1200,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'dist/SoulExplorer/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+ipcMain.on('exit', (event, args) => {
+  mainWindow.close();
+})
+
+ipcMain.on('minimize', (event, args) => {
+  mainWindow.minimize();
+})
+
+ipcMain.on('maximize', (event, args) => {
+  if (mainWindow.isMaximized()) {
+    mainWindow.unmaximize();
+  } else {
+    mainWindow.maximize();
+  }
+})
